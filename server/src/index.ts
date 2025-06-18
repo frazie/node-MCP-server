78c19edfa820c4d3aa9339f40b9e474de33313c2
frazie <ofrazy@gmail.com>
2025-06-18 23:42:57 +0300

    MCP server completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,22 @@ const motorcycles = [
   },
 ];
 
-
+const resources = [
+  {
+    uri: "catalogue://app", //the uri has to match the resource content uri for this to work
+    name: "catalogue",
+    get: async () => {
+      return {
+        contents: [
+          {
+            uri: "catalogue://app",
+            text: JSON.stringify(motorcycles),
+          },
+        ] 
+      }
+    },
+  },
+]
 
 //allow us to take in input and output when communicating with this
 const rl = readline.createInterface({
@@ -50,42 +65,47 @@ const tools = [
     description: "Get the names of all the motorcycles in the shop",
     inputSchema: { type: "object", properties: {} },
     execute: async (args: any) => {
-        return {
-            content : [
-                {
-                    type: "text",
-                    text: JSON.stringify({ names: motorcycles.map((moto) => moto.name) }),
-                }
-            ]
-        }
-    }
+      return {
+        //this format is part of the MCP standard for tools
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify({
+              names: motorcycles.map((moto) => moto.name),
+            }),
+          },
+        ],
+      };
+    },
   },
   {
     name: "getBikeInfo",
     description: "get additional information on your motorcycle of choice",
     inputSchema: {
-        type: "object",
-        properties: {
-            name: {
-                type: "string"
-            }
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
         },
-        required: ["name"]
+      },
+      required: ["name"],
     },
     execute: async (args: any) => {
-        const bike = motorcycles.find((moto) => moto.name === args.name)
+      const bike = motorcycles.find((moto) => moto.name === args.name);
 
-        return {
-            content: [
-                {
-                    type: "text",
-                    text: JSON.stringify(bike || {error: "Bike not found"} )
-                }
-            ]
-        }
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(bike || { error: "Bike of choice not found" }),
+          },
+        ],
+      };
     },
-  }
+  },
 ];
+
+
 
 //our function to run the server
 (async function main() {
@@ -96,25 +116,72 @@ const tools = [
         if (json.method === "initialize") {
           //now we send back the response to our request
           sendReponse(json.id, {
-            protocolversion: "2025-03-26",
+            protocolVersion: "2025-03-26",
             capabilities: {
               tools: { listChanged: true },
+              resources: {listChanged: true },
             },
             serverInfo,
           });
         }
       }
+
       if (json.method === "tools/list") {
         sendReponse(json.id, {
           tools: tools.map((tool) => ({
             name: tool.name,
-            descrription: tool.description,
+            description: tool.description,
             inputSchema: tool.inputSchema,
           })),
         });
       }
+
       if (json.method === "tools/call") {
         const tool = tools.find((tool) => tool.name === json.params.name);
+
+        if (tool) {
+          const toolResponse = await tool.execute(json.params.arguments);
+          sendReponse(json.id, toolResponse);
+        } else {
+          sendReponse(json.id, {
+            error: {
+              code: -32602,
+              message: `MCP error -32602: Tool ${json.params.arguments} not found in the tools list`,
+            },
+          });
+        }
+      }
+
+      //these are needed to support the resources 
+
+      if (json.method === "resources/list") {
+        sendReponse(json.id, {
+          resources: resources.map((resource) => ({
+            uri: resource.uri,
+            name: resource.name,
+            
+          })),
+        });
+      }
+
+      if (json.method === "resources/read") {
+        const uri = json.params.uri
+        const resource = resources.find((resource) => resource.uri === uri)
+
+        if (resource){
+          sendReponse(json.id, await resource.get())
+        }else{
+          sendReponse(json.id, {
+            error: { code: -32602, message: "requested resource not found"}
+          })
+        }
+
+      }
+
+      //as a protocol, this ping method is required for this to meet the standard
+
+      if(json.method === "ping"){
+        sendReponse(json.id, {}) //this can just be blank
       }
     } catch (error) {
       console.error(error);
